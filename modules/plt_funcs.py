8bcb3fb067e0a0f9a49611969b0659bcb807c836
Altug Yildirim <haltugyildirim@users.noreply.github.com>
2021-05-24 00:29:00 +0200

    change of dims
    dimensions of the xarray object changed from delay_corr to Delay and energy_corr to Energy for better readability.

--- modules/plt_funcs.py
+++ modules/plt_funcs.py
@@ -31,14 +31,14 @@ def cdad_shc_just_func(data_right_binned, data_left_binned, min_plot_num_en, max
 
     if min_plot_num_en == max_plot_num_en:
         data_rightsel = data_right_binned.sel(
-            energy_corr=min_plot_num_en, method="nearest")
+            Energy=min_plot_num_en, method="nearest")
         data_leftsel = data_left_binned.sel(
-            energy_corr=min_plot_num_en, method="nearest")
+            Energy=min_plot_num_en, method="nearest")
     else:
-        data_rightsel = data_right_binned.sel(energy_corr=slice(
-            min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr')
-        data_leftsel = data_left_binned.sel(energy_corr=slice(
-            min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr')
+        data_rightsel = data_right_binned.sel(Energy=slice(
+            min_plot_num_en, max_plot_num_en)).sum(dim='Energy')
+        data_leftsel = data_left_binned.sel(Energy=slice(
+            min_plot_num_en, max_plot_num_en)).sum(dim='Energy')
 
     cdad_left_right = (data_rightsel-data_leftsel)/(data_rightsel+data_leftsel)
     return cdad_left_right
@@ -59,17 +59,17 @@ def cdad_svc_just_func(data_right_binned, data_left_binned, min_plot_num_en, max
 
         if max_plot_num_deg == min_plot_num_deg:
             data_rightsel = data_right_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 y=max_plot_num_deg, method="nearest")
             data_leftsel = data_left_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 y=max_plot_num_deg, method="nearest")
         else:
             data_rightsel = data_right_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 y=slice(min_plot_num_deg, max_plot_num_deg)).sum(dim='y')
             data_leftsel = data_left_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 y=slice(min_plot_num_deg, max_plot_num_deg)).sum(dim='y')
         cdad_left_right = (data_rightsel-data_leftsel) / \
             (data_rightsel+data_leftsel)
@@ -78,17 +78,17 @@ def cdad_svc_just_func(data_right_binned, data_left_binned, min_plot_num_en, max
 
         if max_plot_num_deg == min_plot_num_deg:
             data_rightsel = data_right_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 x=max_plot_num_deg, method="nearest")
             data_leftsel = data_left_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 x=max_plot_num_deg, method="nearest")
         else:
             data_rightsel = data_right_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 x=slice(min_plot_num_deg, max_plot_num_deg)).sum(dim='x')
             data_leftsel = data_left_binned.sel(
-                    energy_corr=slice(min_plot_num_en, max_plot_num_en)).sel(
+                    Energy=slice(min_plot_num_en, max_plot_num_en)).sel(
                 x=slice(min_plot_num_deg, max_plot_num_deg)).sum(dim='x')
         cdad_left_right = (data_rightsel-data_leftsel) / \
             (data_rightsel+data_leftsel)
@@ -199,14 +199,14 @@ def cdad_plot(data_right, data_left, length_subplot_in, min_plot_num_en, max_plo
 
         if sing_val != 'none':
             data_rightsel = data_right_binned.sel(
-                energy_corr=z, method="nearest")
+                Energy=z, method="nearest")
             data_leftsel = data_left_binned.sel(
-                energy_corr=z, method="nearest")
+                Energy=z, method="nearest")
         else:
             data_rightsel = data_right_binned.sel(
-                energy_corr=slice(z, z+z_factor)).sum(dim='energy_corr')
+                Energy=slice(z, z+z_factor)).sum(dim='Energy')
             data_leftsel = data_left_binned.sel(
-                energy_corr=slice(z, z+z_factor)).sum(dim='energy_corr')
+                Energy=slice(z, z+z_factor)).sum(dim='Energy')
 
         cdad_left_right = (data_rightsel-data_leftsel) / \
             (data_rightsel+data_leftsel)
@@ -324,14 +324,14 @@ def cdad_shc(data_right_binned, data_left_binned, min_plot_num_en, max_plot_num_
 
     if min_plot_num_en == max_plot_num_en:
         data_rightsel = data_right_binned.sel(
-            energy_corr=min_plot_num_en, method="nearest")
+            Energy=min_plot_num_en, method="nearest")
         data_leftsel = data_left_binned.sel(
-            energy_corr=min_plot_num_en, method="nearest")
+            Energy=min_plot_num_en, method="nearest")
     else:
-        data_rightsel = data_right_binned.sel(energy_corr=slice(
-            min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr')
-        data_leftsel = data_left_binned.sel(energy_corr=slice(
-            min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr')
+        data_rightsel = data_right_binned.sel(Energy=slice(
+            min_plot_num_en, max_plot_num_en)).sum(dim='Energy')
+        data_leftsel = data_left_binned.sel(Energy=slice(
+            min_plot_num_en, max_plot_num_en)).sum(dim='Energy')
 
     cdad_left_right = (data_rightsel-data_leftsel)/(data_rightsel+data_leftsel)
 
@@ -444,8 +444,8 @@ def cdad_dc_comparison(data_right, data_left, x_cut, y_cut, min_plot_num_updc,
                   (min_plot_num_lowdc, max_plot_num_lowdc))
 
     fig10.add_subplot(gs02[0, 0])
-    data_right.sel(energy_corr=slice(min_plot_num_updc, max_plot_num_updc)).sum(
-        dim='energy_corr').plot()
+    data_right.sel(Energy=slice(min_plot_num_updc, max_plot_num_updc)).sum(
+        dim='Energy').plot()
     plt.axhline(y_cut, color='r', linestyle='--', linewidth=2.5)
     plt.axvline(x_cut, color='orange', linestyle='--', linewidth=2.5)
     plt.xlabel('$ \Phi_x$')
@@ -456,8 +456,8 @@ def cdad_dc_comparison(data_right, data_left, x_cut, y_cut, min_plot_num_updc,
         plt.title('%5.2feV to%5.2feV' % (min_plot_num_updc, max_plot_num_updc))
 
     fig10.add_subplot(gs02[1, 0])
-    data_right.sel(energy_corr=slice(min_plot_num_dc, max_plot_num_dc)).sum(
-        dim='energy_corr').plot()
+    data_right.sel(Energy=slice(min_plot_num_dc, max_plot_num_dc)).sum(
+        dim='Energy').plot()
     plt.xlabel('$ \Phi_x$')
     plt.ylabel('$ \Theta_y$')
     if min_plot_num_dc == max_plot_num_dc:
@@ -466,8 +466,8 @@ def cdad_dc_comparison(data_right, data_left, x_cut, y_cut, min_plot_num_updc,
         plt.title('%5.2feV to%5.2feV' % (min_plot_num_dc, max_plot_num_dc))
 
     fig10.add_subplot(gs02[2, 0])
-    data_right.sel(energy_corr=slice(min_plot_num_lowdc, max_plot_num_lowdc)).sum(
-        dim='energy_corr').plot()
+    data_right.sel(Energy=slice(min_plot_num_lowdc, max_plot_num_lowdc)).sum(
+        dim='Energy').plot()
     plt.xlabel('$ \Phi_x$')
     plt.ylabel('$ \Theta_y$')
     if min_plot_num_lowdc == max_plot_num_lowdc:
@@ -634,10 +634,10 @@ def cdad_dc_comparison_full(data_right, data_left, min_plot_num_deg_x, max_plot_
 
     fig10.add_subplot(gs03[0, 0])
     if min_plot_num_updc == max_plot_num_updc:
-        data_right.sel(energy_corr=min_plot_num_updc, method="nearest").plot()
+        data_right.sel(Energy=min_plot_num_updc, method="nearest").plot()
     else:
-        data_right.sel(energy_corr=slice(min_plot_num_updc, max_plot_num_updc)).sum(
-            dim='energy_corr').plot()
+        data_right.sel(Energy=slice(min_plot_num_updc, max_plot_num_updc)).sum(
+            dim='Energy').plot()
 
     plt.axhline(max_plot_num_deg_y, color='r', linestyle='--', linewidth=2.5)
     plt.axhline(min_plot_num_deg_y, color='r', linestyle='--', linewidth=2.5)
@@ -655,10 +655,10 @@ def cdad_dc_comparison_full(data_right, data_left, min_plot_num_deg_x, max_plot_
 
     fig10.add_subplot(gs03[1, 0])
     if min_plot_num_dc == max_plot_num_dc:
-        data_right.sel(energy_corr=min_plot_num_dc, method="nearest").plot()
+        data_right.sel(Energy=min_plot_num_dc, method="nearest").plot()
     else:
-        data_right.sel(energy_corr=slice(min_plot_num_dc, max_plot_num_dc)).sum(
-            dim='energy_corr').plot()
+        data_right.sel(Energy=slice(min_plot_num_dc, max_plot_num_dc)).sum(
+            dim='Energy').plot()
 
     plt.xlabel('$ \Phi_x$')
     plt.ylabel('$ \Theta_y$')
@@ -669,10 +669,10 @@ def cdad_dc_comparison_full(data_right, data_left, min_plot_num_deg_x, max_plot_
 
     fig10.add_subplot(gs03[2, 0])
     if min_plot_num_lowdc == max_plot_num_lowdc:
-        data_right.sel(energy_corr=min_plot_num_lowdc, method="nearest").plot()
+        data_right.sel(Energy=min_plot_num_lowdc, method="nearest").plot()
     else:
-        data_right.sel(energy_corr=slice(min_plot_num_lowdc, max_plot_num_lowdc)).sum(
-            dim='energy_corr').plot()
+        data_right.sel(Energy=slice(min_plot_num_lowdc, max_plot_num_lowdc)).sum(
+            dim='Energy').plot()
 
     plt.xlabel('$ \Phi_x$')
     plt.ylabel('$ \Theta_y$')
@@ -707,10 +707,10 @@ def image_plot(data_binned, length_subplot_in, min_plot_num_en, max_plot_num_en,
             z = i*z_factor+min_plot_num_deg
 
             if sing_val != 'none':
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(y=z, method="nearest")
             else:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(y=slice(z, z+z_factor)).sum(dim='y')
 
             if vmax_val == 'none':
@@ -743,10 +743,10 @@ def image_plot(data_binned, length_subplot_in, min_plot_num_en, max_plot_num_en,
             z = i*z_factor+min_plot_num_deg
 
             if sing_val != 'none':
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(x=z, method="nearest")
             else:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(x=slice(z, z+z_factor)).sum(dim='x')
 
             if vmax_val == 'none':
@@ -785,10 +785,10 @@ def image_plot(data_binned, length_subplot_in, min_plot_num_en, max_plot_num_en,
         z = i*z_factor+min_plot_num_en
 
         if sing_val != 'none':
-            datasel = data_binned.sel(energy_corr=z, method="nearest")
+            datasel = data_binned.sel(Energy=z, method="nearest")
         else:
-            datasel = data_binned.sel(energy_corr=slice(
-                z, z+z_factor)).sum(dim='energy_corr')
+            datasel = data_binned.sel(Energy=slice(
+                z, z+z_factor)).sum(dim='Energy')
 
         if vmax_val == 'none':
             datasel.plot(vmin=0)
@@ -847,10 +847,10 @@ def image_svc(data_binned, min_plot_num_en, max_plot_num_en,
             z = i*z_factor+min_plot_num_deg_y
 
             if max_plot_num_deg_y == min_plot_num_deg_y:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(y=z, method="nearest")
             else:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(y=slice(z, z+z_factor)).sum(dim='y')
 
             if vmax_val == 'none':
@@ -873,10 +873,10 @@ def image_svc(data_binned, min_plot_num_en, max_plot_num_en,
             z = i*z_factor+min_plot_num_deg_x
 
             if max_plot_num_deg_x == min_plot_num_deg_x:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(x=z, method="nearest")
             else:
-                datasel = data_binned.sel(energy_corr=slice(
+                datasel = data_binned.sel(Energy=slice(
                     min_plot_num_en, max_plot_num_en)).sel(x=slice(z, z+z_factor)).sum(dim='x')
 
             if vmax_val == 'none':
@@ -902,10 +902,10 @@ def image_shc(data_binned, min_plot_num_en, max_plot_num_en, vmax_val='none'):
 
     if min_plot_num_en == max_plot_num_en:
         datasel = data_binned.sel(
-            energy_corr=min_plot_num_en, method="nearest")
+            Energy=min_plot_num_en, method="nearest")
     else:
-        datasel = data_binned.sel(energy_corr=slice(
-            min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr')
+        datasel = data_binned.sel(Energy=slice(
+            min_plot_num_en, max_plot_num_en)).sum(dim='Energy')
 
     if vmax_val == 'none':
         datasel.plot(aspect=1.25, size=5, vmin=0)
@@ -924,18 +924,18 @@ def image_shc(data_binned, min_plot_num_en, max_plot_num_en, vmax_val='none'):
 def onelinecut(data, energy_slice_down, energy_slice_up, line_cut_x, line_cut_y, central_angle_x=0, central_angle_y=0):
 
     if energy_slice_down == energy_slice_up:
-        selecteddata_x = data.sel(energy_corr=energy_slice_down, method="nearest").sel(
+        selecteddata_x = data.sel(Energy=energy_slice_down, method="nearest").sel(
             y=line_cut_x, method="nearest").values
-        selecteddata_y = data.sel(energy_corr=energy_slice_down, method="nearest").sel(
+        selecteddata_y = data.sel(Energy=energy_slice_down, method="nearest").sel(
             x=line_cut_y, method="nearest").values
     else:
-        selecteddata_x = data.sel(energy_corr=slice(energy_slice_down, energy_slice_up)).sum(
-            dim='energy_corr').sel(y=line_cut_x, method="nearest").values
-        selecteddata_y = data.sel(energy_corr=slice(energy_slice_down, energy_slice_up)).sum(
-            dim='energy_corr').sel(x=line_cut_y, method="nearest").values
+        selecteddata_x = data.sel(Energy=slice(energy_slice_down, energy_slice_up)).sum(
+            dim='Energy').sel(y=line_cut_x, method="nearest").values
+        selecteddata_y = data.sel(Energy=slice(energy_slice_down, energy_slice_up)).sum(
+            dim='Energy').sel(x=line_cut_y, method="nearest").values
     x_data = data.coords['x'].values-central_angle_x
     y_data = data.coords['y'].values-central_angle_y
-    en_data = data.coords['energy_corr'].values
+    en_data = data.coords['Energy'].values
     energy_val = (energy_slice_up+energy_slice_down)/2
     k_x = np.sqrt(2*m_e*energy_val*e)*(np.sin(x_data*np.pi/180)/hbar)
     k_y = np.sqrt(2*m_e*energy_val*e)*(np.sin(y_data*np.pi/180)/hbar)
@@ -974,10 +974,10 @@ def onelinecut(data, energy_slice_down, energy_slice_up, line_cut_x, line_cut_y,
     fig10.add_subplot(gs01[0, 0])
 
     if energy_slice_down == energy_slice_up:
-        data.sel(energy_corr=energy_slice_down, method="nearest").plot()
+        data.sel(Energy=energy_slice_down, method="nearest").plot()
     else:
-        data.sel(energy_corr=slice(energy_slice_down, energy_slice_up)).sum(
-            dim='energy_corr').plot()
+        data.sel(Energy=slice(energy_slice_down, energy_slice_up)).sum(
+            dim='Energy').plot()
     plt.title('Energy Cut')
 
     plt.axhline(line_cut_x, color='r', linestyle='--')
@@ -1003,28 +1003,35 @@ def onelinecut(data, energy_slice_down, energy_slice_up, line_cut_x, line_cut_y,
     return
 
 
-def comparison_plot(data_right,data_left,min_plot_num_en,max_plot_num_en, x = 0, y = 0, dirac_energy = 0.15):
+def comparison_plot(data_right,data_left,min_plot_num_en,max_plot_num_en, x = 0, y = 0, dirac_energy = 0.15, single_plot = 'on'):
     """
     This for checking the newly converted data's correction values and orientation.
     """
     fig, axs = plt.subplots(2,2, figsize=(12,10))
 
-    data_right.sum(dim=('x','y')).plot(ax=axs[0,0],label='$ 0  \degree \circlearrowright$')
-    data_left.sum(dim= ('x','y')).plot(ax=axs[0,0],label='$ 90 \degree \circlearrowleft$')
-    axs[0,0].set_xlabel('$E-E_F (eV)$')
-    axs[0,0].set_ylabel('counts (a.u.)')
-    axs[0,0].legend()
-    axs[0,0].set_title(label = '')
+    if single_plot != 'none':
+        data_right.sum(dim=('x','y')).plot(ax=axs[0,0],label='$ 0  \degree \circlearrowright$')
+        data_left.sum(dim= ('x','y')).plot(ax=axs[0,0],label='$ 90 \degree \circlearrowleft$')
+        axs[0,0].set_xlabel('$E-E_F (eV)$')
+        axs[0,0].set_ylabel('counts (a.u.)')
+        axs[0,0].legend()
+        axs[0,0].set_title(label = '')
+    else:
+        data_right.sum(dim=('x','y')).plot(ax=axs[0,0],label='data')
+        axs[0,0].set_xlabel('$E-E_F (eV)$')
+        axs[0,0].set_ylabel('counts (a.u.)')
+        axs[0,0].legend()
+        axs[0,0].set_title(label = '')
 
-    data_right.sel(energy_corr=slice(
-        min_plot_num_en, max_plot_num_en)).sum(dim='energy_corr').plot(ax=axs[1,0])
+    data_right.sel(Energy=slice(
+        min_plot_num_en, max_plot_num_en)).sum(dim='Energy').plot(ax=axs[1,0])
     axs[1,0].axvline(x = 0 ,color = 'r', linestyle = '--')
     axs[1,0].axhline(y = 0 ,color = 'r', linestyle = '--')
     axs[1,0].set_title(label = '')
     axs[1,0].set_xlabel('$k_{ x}(\AA^{-1})$')
     axs[1,0].set_ylabel('$k_{ y}(\AA^{-1})$')
 
-    data_right.sel(energy_corr=slice(0.08, 0.5)).sel(y=y, method="nearest").plot(ax=axs[0,1])
+    data_right.sel(Energy=slice(0.08, 0.5)).sel(y=y, method="nearest").plot(ax=axs[0,1])
     axs[0,1].axvline(x = 0 ,color = 'r', linestyle = '--')
     axs[0,1].axhline(y = dirac_energy ,color = 'r', linestyle = '--')
     axs[0,1].axhline(y = min_plot_num_en ,color = 'g', linestyle = '--')
@@ -1033,7 +1040,7 @@ def comparison_plot(data_right,data_left,min_plot_num_en,max_plot_num_en, x = 0,
     axs[0,1].set_ylabel('$E-E_F (eV)$')
     axs[0,1].set_xlabel('$k_{ y}(\AA^{-1})$')
 
-    data_right.sel(energy_corr=slice(0.08, 0.5)).sel(x=x, method="nearest").plot(ax=axs[1,1])
+    data_right.sel(Energy=slice(0.08, 0.5)).sel(x=x, method="nearest").plot(ax=axs[1,1])
     axs[1,1].axvline(x = 0 ,color = 'r', linestyle = '--')
     axs[1,1].axhline(y = dirac_energy ,color = 'r', linestyle = '--')
     axs[1,1].set_title(label = '')
@@ -1042,5 +1049,5 @@ def comparison_plot(data_right,data_left,min_plot_num_en,max_plot_num_en, x = 0,
 
 
     plt.tight_layout()
-    
+
     return
